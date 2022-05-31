5488cc245a358bc8b518e3de294673df3c63e5c7
nstmanish <manish.chourasiya@neosoftmail.com>
2022-05-31 11:24:36 +0530

    review changes

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,25 +1,30 @@
 const employeeModel = require('../models/employee');
 
-exports.add_employee = async (req, res) => {
-    await employeeModel.find({ email : req.body.email })
-    .exec(function (err, data){
-        if (err) { return next(err); }
-        if ( data.length > 0 ){ return res.json({message:"Already exist", data}) }
-        employeeModel.create({ 
+exports.addEmployee = async (req, res) => {
+
+    const userExist = await employeeModel.findOne({ email : req.body.email })
+
+    if (userExist) {
+        return res.status(409).json({message:"user Already Exist", data:userExist });
+    }
+
+    try {
+        const employee = await employeeModel.create({ 
             empName : req.body.empName,  
             empdept : req.body.empdept,
             mobile  : req.body.mobile,
             email   : req.body.email , 
             role    : req.body.role,
             salary  : req.body.salary
-        })
-        .then(function(data) { 
-            res.json({message:"Successfull", data});
-        });        
-    });
+        });
+        res.status(200).json({message:"Successfull", data:employee });
+    }catch (err) {
+        console.log(err);
+    }
+
 }
 
-exports.List_employee = (req, res) => {
+exports.ListEmployee = (req, res) => {
     employeeModel.find({})
     .exec(function (err, data){
         if (err) { return next(err); }
@@ -27,7 +32,7 @@ exports.List_employee = (req, res) => {
     });
 }
 
-exports.view_employee = (req, res) => {
+exports.viewEmployee = (req, res) => {
     employeeModel.findOne({ _id : req.params.employeeId })
     .exec(function (err, data){
         if (err) { return next(err); }
@@ -35,7 +40,7 @@ exports.view_employee = (req, res) => {
     });
 }
 
-exports.update_employee  = (req, res) => {
+exports.updateEmployee  = (req, res) => {
     employeeModel.updateOne({ _id : req.params.employeeId },
         {
             empName : req.body.empName,  
@@ -52,7 +57,7 @@ exports.update_employee  = (req, res) => {
     });
 }
 
-exports.delete_employee = (req, res) => {
+exports.deleteEmployee = (req, res) => {
     employeeModel.deleteOne({ _id : req.params.employeeId })
     .exec(function (err, data){
         if (err) { return next(err); }
@@ -60,7 +65,7 @@ exports.delete_employee = (req, res) => {
     });
 }
 
-exports.salary_lt_employee  = (req, res) => {
+exports.salaryItEmployee  = (req, res) => {
     employeeModel.find({ salary : { $lt: req.params.lessThan }})
     .exec(function (err, list){
         if (err) { return next(err); }
--- routes/employee.js
+++ routes/employee.js
@@ -2,14 +2,14 @@ let express = require('express');
 let router = express.Router();
 
 /** Exporting The employee Controller */
-let employeeController = require('../Controllers/employeeController');
+let employeeController = require('../controllers/employeeController');
 
 /** Employee Routes */
-router.post  ('/new'                , employeeController.add_employee       );
-router.get   ('/employeeList'       , employeeController.List_employee      );
-router.get   ('/detail/:employeeId' , employeeController.view_employee      );
-router.put   ('/update/:employeeId' , employeeController.update_employee    );
-router.delete('/delete/:employeeId' , employeeController.delete_employee    );
-router.get   ('/salary/:lessThan'   , employeeController.salary_lt_employee );
+router.post  ('/new'                , employeeController.addEmployee       );
+router.get   ('/employeeList'       , employeeController.ListEmployee      );
+router.get   ('/detail/:employeeId' , employeeController.viewEmployee      );
+router.put   ('/update/:employeeId' , employeeController.updateEmployee    );
+router.delete('/delete/:employeeId' , employeeController.deleteEmployee    );
+router.get   ('/salary/:lessThan'   , employeeController.salaryItEmployee  );
 
 module.exports = router;
